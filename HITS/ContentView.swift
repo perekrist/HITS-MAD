7799e339e9d3e09dc19e48a824edf2c1a8bef9b3
Kristina Peregudova <k.peregudova@redmadrobot.com>
2022-09-28 15:00:39 +0700

    Initial Commit

---
+++ HITS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HITS
+//
+//  Created by Кристина Перегудова on 28.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HITS/HITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HITSApp.swift
+//  HITS
+//
+//  Created by Кристина Перегудова on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
